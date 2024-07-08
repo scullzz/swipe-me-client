ab395a6f9b680b7d79b91ee22e4685d969d2288f
Ahad Askarov <aaskarov870@gmail.com>
2024-07-08 21:57:41 +0500

    info

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Main from "./components/main/Main";
+import { InfoPage } from "./components/InfoPage/InfoPage";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Router>
     <Routes>
       <Route Component={Main} path="/"></Route>
+      <Route Component={InfoPage} path="/info"></Route>
     </Routes>
   </Router>
 );
