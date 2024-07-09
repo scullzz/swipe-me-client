1935abe2083a26c6784ca437790570d78599cd5e
Ahad Askarov <aaskarov870@gmail.com>
2024-07-09 11:23:47 +0500

    started to dev tasks in info-page

--- src/components/info-page/task-components/Task.jsx
+++ src/components/info-page/task-components/Task.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import style from "./TaskStyle.module.css"
+import done_task from  "../images/blue_task.svg"
+import closed_arrow from "../images/closed_arrow.svg"
 
 const Task = () => {
   return (
-    <div>
-      
+    <div className={style.task}>
+      <div className={style.task_main_info}>
+        <img src={done_task} alt="done_task" className={style.task_icon_status} />
+        <div className={style.title_date_wrapper}>
+          <p className={style.task_title}>Name task</p>
+          <p className={style.task_date_complete}>14 september 14:06</p> 
+        </div>
+      </div>
+      <div className={style.arrow_status_q_swapper}>
+        <p className={style.task_q_year}>Q1 2024</p>
+        <button className={style.closed_arrow_btn} onClick={() => {}}>
+          <img src={closed_arrow} alt="open_arrow" />
+        </button>
+      </div>
     </div>
   )
 }
