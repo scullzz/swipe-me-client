9ee916e21d57c26e107678f027ce6a0186c23ca7
scullzz <rasulhuzaevsaid@gmail.com>
2024-07-09 20:09:00 +0500

    fix

---
+++ src/components/cooperation/Cooperation.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import style from "./style.module.css";
+const Cooperation = () => {
+  return (
+    <div className={style.Main_Cooperation}>
+      <p className={style.Cooperation_Text}>For Cooperation</p>
+      <a className={style.Cooperation_Link} href="#">
+        @mavruss
+      </a>
+    </div>
+  );
+};
+
+export default Cooperation;
