dc66623cce4edd105b11af05e3fe5cfb25976fc0
mgwr <aaskarov870@gmail.com>
2024-07-28 23:21:09 +0500

    some changes

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,5 +1,12 @@
 import React, { useEffect, useState, useRef } from "react";
 import style from "./style.module.css";
+import muted_sound_icon from "./images/muted_sound.svg";
+import unmuted_sound_icon from "./images/unmuted_sound.svg";
+import avatar from "./images/avatar.svg";
+import heart from "./images/like_heart.svg";
+import comments from "./images/comments.svg";
+import share from "./images/share.svg";
+import plus_icon from "./images/plus.svg";
 
 const Home = () => {
   const [isPlaying, setIsPlaying] = useState(true);
@@ -12,6 +19,7 @@ const Home = () => {
     "/videos/cat sys admin.mp4",
     "/videos/saids_defauld_day.mp4",
     "/videos/sunflower.mp4",
+    "/videos/movie.m4v",
   ];
 
   const [sub_video, set_sub_video_src] = useState([]);
@@ -53,13 +61,54 @@ const Home = () => {
       <div className={style.video_player_wrapper} onClick={handlePlayPause}>
         <video
           ref={videoRef}
-          src={videosSrc[4]}
+          src={videosSrc[1]}
           autoPlay={true}
           loop={true}
           muted={isMuted}
           controls={false}
           className={style.video_player}
         />
+      </div>
+      <button className={style.mute_btn}>
+          <img src={unmuted_sound_icon} alt="unmuted_sound_icon" />
+      </button>
+      <div className={style.overlay_top}>
+        <div className={style.overlay_top_content_selection}>
+          <button>Подписки</button>
+          <button>Новинки</button>
+        </div>
+      </div>
+      <div className={style.overlay_right}>
+          <div className={style.overlay_right_content}>
+            <div className={style.overlay_right_content_part}>
+              <button className={style.overlay_right_content_button}>
+                <img src={avatar} alt="avatar" />
+              </button>
+              <button className={style.overlay_right_avatar_btn}>
+                <img src={plus_icon} alt="plus_icon" />
+              </button>
+            </div>
+            <div className={style.right_btns_wrapper}>
+              <div className={style.overlay_right_content_part}>
+                <button className={style.overlay_right_content_button}>
+                  <img src={heart} alt="heart" />
+                </button>
+                <p>11.2M</p>
+              </div>
+              <div className={style.overlay_right_content_part}>
+                <button className={style.overlay_right_content_button}>
+                  <img src={comments} alt="comments" />
+                </button>
+                <p>11.2M</p>
+              </div>
+              <div className={style.overlay_right_content_part}>
+                <button className={style.overlay_right_content_button}>
+                  <img src={share} alt="share" />
+                </button>
+                <p>11.2M</p>
+              </div>
+            </div>
+          </div>
       </div>
     </div>
   );
