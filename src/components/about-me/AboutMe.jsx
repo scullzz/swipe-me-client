b1fb40657afd449e4907cf12fe2d4c608b722726
scullzz <rasulhuzaevsaid@gmail.com>
2024-08-11 11:19:58 +0500

    last

--- src/components/about-me/AboutMe.jsx
+++ src/components/about-me/AboutMe.jsx
@@ -92,15 +92,13 @@ const AboutMe = () => {
   //test api
   const getTestData = async () => {
     try {
-      const response = await fetch("http://185.250.45.105/accounts/m", {
+      const response = await fetch("http://185.250.45.105/accounts/s", {
         method: "POST",
         headers: {
           "Content-type": "application/json",
           "Telegram-User-ID": userData?.id,
-        },
-        body: {
-          auth: userData,
-        },
+          "Auth": "M1bCSx92W6",
+        }
       });
       const data = await response.json();
       setAuthData(data);
